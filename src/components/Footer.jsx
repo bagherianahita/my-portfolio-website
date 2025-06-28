5b77e585c6d3b0de090999b77bf1e379ce6b80ce
anahita <bagheri.anahita65@gmail.com>
2025-06-28 09:27:00 -0230

    Update footer and hero with new link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
             <div className="container">
                 <p>&copy; {new Date().getFullYear()} Anahita Bagheri Kalhor. All rights reserved.</p>
                 <div className="social-links">
-                    <a href="www.linkedin.com/in/anahita-bagheri-developer" target="_blank">LinkedIn</a>
+                    <a href="www.linkedin.com/in/anahita-bagheri-developer-pmp" target="_blank">LinkedIn</a>
                     {/* Add more social links as needed */}
                 </div>
             </div>
         </footer>
     );
 };
-
+//map st johns
 export default Footer;
